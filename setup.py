ae7caff7397264e6916497fc7f2847a37664fc8a
Alexandre René <alexandre.rene@freeneurons.ca>
2018-06-30 12:45:06 +0200

    Add some dependencies to `setup.py`

--- setup.py
+++ setup.py
@@ -5,6 +5,10 @@ setup(
     version='0.1dev',
     packages=['sdde-dim'],
     install_requires=[
-        'tqdm >= 4.23.4'
+        'tqdm >= 4.23.4',
+        'luigi >= 2.7.5',
+        'sinn >= 0.1',       # Not publicly available yet
+        'acr_tools >= 0.1',  # Not publicly available yet
+        'theano_shim >= 0.2.3'
     ]
 )
